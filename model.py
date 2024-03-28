8d9ff0793ee99a609b975f75510d07114d6d1d51
Jatin Mehra <dev.jatin.singh@gmail.com>
2024-03-28 22:08:54 +0530

    Created Web App Using Streamlit (not deployed)

---
+++ model.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 28 21:37:02 2024
+
+@author: jatin
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 28 20:44:08 2024
+
+@author: jatin
+"""
+
+import numpy as np
+import pickle
+import streamlit as st
+
+loaded_model = pickle.load(open('E:/Heart_Disease_prediction/trained_model.sav','rb'))
+
+#creating a function for prediction
+
+def heart_prediction(input_data):
+
+    input_numpy=np.asarray(input_data)
+    
+    #predicting for one instance
+    input_data_reshape=input_numpy.reshape(1,-1)
+
+    prediction = loaded_model.predict(input_data_reshape)
+
+    if(prediction[0]==0):
+        return 'Congratulations ! You are free from heart disease !'
+    else:
+        return 'I would prefer you should consult you doctor !'
+    
+def main():
+    
+    #giving a title
+    st.title('Heart-Disease-Prediction-App')
+    
+    #getiing input data from user 
+    
+    age = st.text_input('Enter the age of person')
+    sex = st.text_input('Enter the gender')
+    cp = st.text_input('Cp value')
+    trestbps = st.text_input('trestbps value')
+    chol = st.text_input('Cholestrol Value')
+    fbs = st.text_input('FBS value')
+    restecg = st.text_input('RestEcg Value')
+    thalach = st.text_input('Thalach Value')
+    exang = st.text_input('Exang value')
+    oldpeak = st.text_input('Oldpeak value')
+    slope = st.text_input('Slope Value')
+    ca = st.text_input('Ca Value')
+    thal = st.text_input('Thal value')
+    
+    #code for prediction 
+    diagnosis = ""
+    
+    #creating a button
+    
+    if st.button('GENERATE RESULT'):
+       diagnosis = heart_prediction([age,sex,cp,trestbps,chol,fbs,restecg,thalach,exang,oldpeak,slope,ca,thal]) 
+       
+    st.success(diagnosis)
+    
+    
+    
+if __name__=='__main__':
+    main()
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
---
+++ predictivesystem.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 28 21:37:36 2024
+
+@author: jatin
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import numpy as np
+import pickle
+loaded_model = pickle.load(open('E:/Heart_Disease_prediction/trained_model.sav','rb'))
+
+input = (58,1,0,146,218,0,1,105,0,2,1,1,3)
+
+convert_numpy = np.asarray(input)
+
+input_reshape = convert_numpy.reshape(1,-1)
+
+prediction = loaded_model.predict(input_reshape)
+
+if ((prediction[0])==1):
+    print("Person have Heart Disease !")
+else:
+    print("Person is free from heart disease !")
